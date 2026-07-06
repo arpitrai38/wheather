7301d2aebf598c23dbd2bebd4a44451a87d0ab95
Arpit Rai <sadhanamarendra12@gmail.com>
2026-07-07 00:32:24 +0530

    1st project

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+async function search(){
+    let a = document.getElementById("city").value;
+    // console.log(a)
+
+    const key='634d18546ad7d1d421f489c9d8ade896'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${a}&appid=${key}&unit = metric`
+    let res = await fetch(url)
+    let data= await res.json();
+    console.log(data);
+    document.querySelector('.a h3').innerHTML= `Temp = ${data.main.temp} ℃`
+
+   document.querySelector('.a h4').innerHTML = `City = ${data.name}`
+
+    // console.log(res)
+}
+
+
+
+
+
+
+
+
+
+// https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=634d18546ad7d1d421f489c9d8ade896
